320125230c69e81db13ae11caaa0432e6925243f
Luisana Pino <pino.luisana@gmail.com>
2020-04-28 17:21:02 -0400

    actualizacion del readme, solo faltarian lasimagenes de los prototipo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+import pokeData from './data/pokemon/pokemon.js';
+
+
+//console.log(pokeData.pokemon [115])
+
+const select = document.getElementById ("ordenarPokedex")
+
+//select.option[select.selectedIndex].tex
+
+select.addEventListener("change", () => {
+    let condition = select.option[select.selectedIndex].tex
+})
