f7bffdcc7ba540d621f76c425bd66caa76fd18e1
Dominique Rubilar <Dominique-rubilar@github.com>
2020-05-13 12:31:33 -0400

    cambios para hacer deploy

--- src/data.js
+++ src/data.js
@@ -1,11 +1,26 @@
+//funcion para ordenar
 const sortData = (pokeData,sortBy,sortOrder) => {
   let dataOrdenar = pokeData.slice();
   let orderAz = dataOrdenar.sort((a, b) => {
-    return a[sortBy].localeCompare(b[sortBy]); //esta linea compara la propiedad del pokemon a con la misma propiedad (sortBy)
-     })
-     if (sortOrder === "z-a"){
+      return a[sortBy].localeCompare(b[sortBy]); //esta linea compara la propiedad del pokemon a con la misma propiedad (sortBy)
+  })
+  if (sortOrder === "z-a"){
       orderAz.reverse();
-    }
-    return orderAz;
-  };
-  export {sortData};
+  }
+  return orderAz;
+};
+//funcion para filtrar por tipo
+const filterData = (pokeData, condition) => {
+  const filtered = pokeData.filter(element => {
+  return element.type.includes(condition) === true;
+  })
+  return filtered;
+};
+//funcion para filtrar por debilidades
+const filterDataDebilidades  = (pokeData, condition) => {
+  const filtered = pokeData.filter(element => {
+  return element.weaknesses.includes(condition) === true;
+  })
+  return filtered;
+};
+  export {sortData, filterData,filterDataDebilidades};
